9a518ca0ff943746a26bee7cc5975990e82f1276
Uday Hiwarale <Uday.Hiwarale>
2020-09-12 21:09:40 +0530

    equality changes

--- equality/loose-spooky.js
+++ equality/loose-spooky.js
@@ -1,5 +1,5 @@
 // primitive data types
-console.log( '0 == "0" =>', 0 == 0 );
+console.log( '0 == "0" =>', 0 == "0" );
 console.log( '0 == "-0" =>', 0 == "-0" )
 console.log( '1 == "-1" =>', 1 == "-1" );
 console.log( '1 != "1" =>', 1 != "1" );
--- equality/strict.js
+++ equality/strict.js
@@ -1,33 +1,34 @@
 // primitive  data types
-console.log( '0 === 0 =>', 0 === 0 ); // number
-console.log( '0 === -0 =>', 0 === -0 ); // number
-console.log( '1 === 1 =>', 1 === 1 ); // number
-console.log( '1 === -1 =>', 1 === -1 ); // number
-console.log( '1 === 2 =>', 1 === 2 ); // number
-console.log( '"1" === "1" =>', "1" === "1" ); // string
-console.log( '"hello" === "world" =>', "hello" === "world" ); // string
-console.log( 'true === true =>', true === true ); // boolean
-console.log( 'true !== false =>', true !== false ); // boolean
-console.log( 'Symbol() === Symbol() =>', Symbol() === Symbol() ); // symbol
-console.log( 'null === null =>', null === null ); // null
-console.log( 'undefined === undefined =>', undefined === undefined ); // undefined
+console.log( '0 === 0 =>', 0 === 0 );
+console.log( '0 === -0 =>', 0 === -0 );
+console.log( '1 === 1 =>', 1 === 1 );
+console.log( '1 === -1 =>', 1 === -1 );
+console.log( '1 === 2 =>', 1 === 2 );
+console.log( '"1" === "1" =>', "1" === "1" );
+console.log( '"hello" === "world" =>', "hello" === "world" );
+console.log( 'true === true =>', true === true );
+console.log( 'true !== false =>', true !== false );
+console.log( 'Symbol() === Symbol() =>', Symbol() === Symbol() );
+console.log( 'null === null =>', null === null );
+console.log( 'undef === undef =>', undefined === undefined );
+console.log( 'undef === null =>', undefined === null );
 
 /*----------*/
 
 // abstract data types
-console.log( '(function(){}) === (function(){}) =>', (function(){}) === (function(){}) ); // function
-console.log( '{} === {} =>', {} === {} ); // object
-console.log( '{a:1} === {a:1} =>', {a:1} === {a:1} ); // object
-console.log( '[] === [] =>', [] === [] ); // array
-console.log( '[1] === [1] =>', [1] === [1] ); // array
+console.log( '(function(){}) === (function(){}) =>', (function(){}) === (function(){}) );
+console.log( '{} === {} =>', {} === {} );
+console.log( '{a:1} === {a:1} =>', {a:1} === {a:1} );
+console.log( '[] === [] =>', [] === [] );
+console.log( '[1] === [1] =>', [1] === [1] );
 
 var obj = { a: 1 }, arr = [ 1 ], func = function(){};
-console.log( 'obj === obj =>', obj === obj ); // obj
-console.log( 'arr === arr =>', arr === arr ); // arr
-console.log( 'func === func =>', func === func ); // obj
+console.log( 'obj === obj =>', obj === obj );
+console.log( 'arr === arr =>', arr === arr );
+console.log( 'func === func =>', func === func );
 
 /*----------*/
 
 // special cases
-console.log( 'NaN === NaN =>', NaN === NaN ); // NaN
-console.log( 'Infinity === Infinity =>', Infinity === Infinity ); // Infinity
+console.log( 'NaN === NaN =>', NaN === NaN );
+console.log( 'Infinity === Infinity =>', Infinity === Infinity );
